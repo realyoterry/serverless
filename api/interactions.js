e4bf21f0ff2c9ec020f2447e01cec15fe2baa2e3
Terry Kim <theterrykim@gmail.com>
2025-08-02 18:02:08 +1200

    fix

--- api/interactions.js
+++ api/interactions.js
@@ -105,7 +105,7 @@ async function incrementSupport(name) {
 }
 
 async function getLeaderboard() {
-	const result = await redis.zrange('leaderboard', 0, 9, { REV: true, WITHSCORES: true });
+	const result = await redis.zrange('ship_leaderboard', 0, 9, { REV: true, WITHSCORES: true });
 
 	return result;
 }
